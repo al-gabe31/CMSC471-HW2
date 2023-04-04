8ed438507a25b63ee75aede2ad37936139f6588b
Gabe Aquino <lq90575@umbc.edu>
2023-04-04 17:08:34 -0400

    Greedy Best First looks good

--- functions.py
+++ functions.py
@@ -4,6 +4,9 @@ from queue import PriorityQueue # Needed for Priority Queue
 X_CAPACITY = 3 # Capacity of the first water jug
 Y_CAPACITY = 1 # Capacity of the second water jug
 
+X_START = 2
+Y_START = 1
+
 class waterState:
     def __init__(self, x = X_CAPACITY, y = Y_CAPACITY, prevState = None):
         # Validates constructor inputs
@@ -75,7 +78,7 @@ class WaterPouringSolution:
         visited = []
         frontier = deque() # Will be a Queue data structure
 
-        frontier.append(waterState(3, 1))
+        frontier.append(waterState(X_START, Y_START))
         curr_state = frontier[0]
 
         iters = 0 # Will also act as the cost
@@ -120,7 +123,7 @@ class WaterPouringSolution:
         visited = []
         frontier = deque() # Will be a Queue data structure
 
-        frontier.append(waterState(3, 1))
+        frontier.append(waterState(X_START, Y_START))
         curr_state = frontier[0]
 
         iters = 0 # Will also act as the cost
@@ -144,6 +147,51 @@ class WaterPouringSolution:
             
             # Gets state from the left of the Queue
             curr_state = frontier.pop()
+
+            iters += 1
+
+        # Obtain the solution path
+        solution_path = [curr_state]
+        while(solution_path[-1].prevState):
+            solution_path.append(solution_path[-1].prevState)
+        solution_path.reverse()
+        
+        return (solution_path, iters)
+    
+    # Performs a greedy best-first search using h
+    # Returns a tuple where index:
+    #   - 0: End state
+    #   - 1: Cost of solution
+    @staticmethod
+    def best_first():
+        visited_string = []
+        visited = []
+        frontier = PriorityQueue() # Will be a Priority Queue data structure
+
+        frontier.append(waterState(X_START, Y_START))
+        curr_state = frontier[0]
+
+        iters = 0 # Will also act as the cost
+        max_iters = 1000
+
+        # Keeps finding a solution until:
+        #   - Frontier is empty (solution isn't possible)
+        #   - Goal state found (heuristic value = 0)
+        while frontier.qsize > 0 and curr_state.get_heuristic() != 0 and iters < max_iters:
+            # Adds current state to the visited list (if it hasn't been visited yet)
+            if curr_state.string_repr() not in visited_string:
+                visited_string.append(curr_state.string_repr())
+                visited.append(curr_state)
+            
+            # Adds new states to the frontier based on all possible actions
+            for state in curr_state.perform_all():
+                if state.string_repr() not in visited_string:
+                    frontier.put(state)
+                
+                # frontier.append(state)
+            
+            # Gets state from the left of the Queue
+            curr_state = frontier.get()
 
             iters += 1
 
