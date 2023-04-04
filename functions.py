01eeb798c533b0db524043e9236be050c127d398
Gabe Aquino <lq90575@umbc.edu>
2023-04-04 16:18:55 -0400

    Finished Breadth First for Question 3

--- functions.py
+++ functions.py
@@ -32,11 +32,13 @@ class waterState:
     
     # Pours the 3 gallon jug to the 1 gallon jug
     def pour_x_to_y(self):
-        return waterState(0, min(Y_CAPACITY, self.x + self.y), self)
+        num_poured = min(self.x, Y_CAPACITY - self.y)
+        return waterState(self.x - num_poured, min(Y_CAPACITY, self.x + self.y), self)
     
     # Pours the 1 gallon jug to the 3 gallon jug
     def pour_y_to_x(self):
-        return waterState(min(X_CAPACITY, self.x + self.y), 0, self)
+        num_poured = min(X_CAPACITY - self.x, self.y)
+        return waterState(min(X_CAPACITY, self.x + self.y), self.y - num_poured, self)
     
     # Performs all 4 possible actions
     # Returns a tuple
@@ -51,6 +53,10 @@ class waterState:
     def __str__(self):
         return f"X = {self.x} & Y = {self.y}"
     
+    # String representation of the state
+    def string_repr(self):
+        return f"{self.x}:{self.y}"
+    
     # Makes waterState objects comparable to each other using the getHeuristic method
     def __lt__(self, otherState):
         return waterState.get_heuristic(self) < waterState.get_heuristic(otherState)
@@ -59,27 +65,41 @@ class waterState:
         return waterState.get_heuristic(self) == waterState.get_heuristic(otherState)
 
 class WaterPouringSolution:
+    # Performs a breadth-first solution on the problem
+    # Returns a tuple where index:
+    #   - 0: End state
+    #   - 1: Cost of solution
     @staticmethod
     def breadth_first():
+        visited_string = []
         visited = []
         frontier = deque() # Will be a Queue data structure
 
         frontier.append(waterState(3, 1))
         curr_state = frontier[0]
 
+        iters = 0 # Will also act as the cost
+        max_iters = 1000
+
         # Keeps finding a solution until:
         #   - Frontier is empty (solution isn't possible)
         #   - Goal state found (heuristic value = 0)
-        while len(frontier) > 0 and curr_state.get_heuristic() != 0:
+        while len(frontier) > 0 and curr_state.get_heuristic() != 0 and iters < max_iters:
             # Adds current state to the visited list (if it hasn't been visited yet)
-            if curr_state not in visited:
+            if curr_state.string_repr() not in visited_string:
+                visited_string.append(curr_state.string_repr())
                 visited.append(curr_state)
             
             # Adds new states to the frontier based on all possible actions
             for state in curr_state.perform_all():
-                frontier.append(state)
+                if state.string_repr() not in visited_string:
+                    frontier.append(state)
+                
+                # frontier.append(state)
             
             # Gets state from the left of the Queue
             curr_state = frontier.popleft()
+
+            iters += 1
         
-        return curr_state
+        return (curr_state, iters)
